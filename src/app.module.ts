9540d46d311cf9cdb86e8db50e8c14c9f328d3f4
ahmed ahmed <engineerahmed413@gmail.com>
2025-10-13 22:56:29 +0300

    migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,43 +3,45 @@ import { productsModule } from './products/products.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './products/product.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { User } from './users/user.entity';
-import { Review } from './reviews/review.entity';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
+import { dataSourceOptions } from '../db/data-source';
 
 
 @Module({
-  controllers : [AppController] ,
+  controllers: [AppController],
   imports: [
     productsModule,
     ReviewsModule,
     UsersModule,
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'postgres',
-          database: config.get<string>("DB_DATABASE"),
-          username: config.get<string>("DB_USERNAME"),
-          password: config.get<string>("DB_PASSWORD"),
-          port: config.get<number>("DB_PORT"),
-          host: 'localhost',
-          synchronize: process.env.NODE_ENV !== 'prodcution',
-          entities: [Product , User , Review]
-        }
-      }
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV !== 'production' ? `.env.${process.env.NODE_ENV}` : '.env'
     })
   ],
-  providers :[{
-    provide : APP_INTERCEPTOR ,
-    useClass : ClassSerializerInterceptor
+  providers: [{
+    provide: APP_INTERCEPTOR,
+    useClass: ClassSerializerInterceptor
   }]
 })
 export class AppModule { }
+
+
+/*   LOCAL DATABASE */
+// {
+//       inject: [ConfigService],
+//       useFactory: (config: ConfigService) => {
+//         return {
+//           type: 'postgres',
+//           database: config.get<string>("DB_DATABASE"),
+//           username: config.get<string>("DB_USERNAME"),
+//           password: config.get<string>("DB_PASSWORD"),
+//           port: config.get<number>("DB_PORT"),
+//           host: 'localhost',
+//           synchronize: process.env.NODE_ENV !== 'prodcution',
+//           entities: [Product , User , Review]
+//         }
+//       }
+// }
